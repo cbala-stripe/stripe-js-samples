1a6fa44a0eb80c366f1b99ec33cabf158e3422d5
Christopher Henn <christopher@stripe.com>
2021-10-11 07:37:21 -0400

    Add collapse button to controls

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { PAGES } from "../constants";
 import { SampleSelect } from "./SampleSelect";
 
@@ -5,6 +7,8 @@ export const Layout: React.FC<{ controls?: JSX.Element }> = ({
   children,
   controls = null,
 }) => {
+  const [showControls, setShowControls] = useState(true);
+
   const options = PAGES.map(({ title, href }) => ({
     value: href,
     label: title,
@@ -12,9 +16,19 @@ export const Layout: React.FC<{ controls?: JSX.Element }> = ({
 
   return (
     <>
-      <header className="bg-gray-100 pt-6 pb-8 px-8 border-b border-gray-200 grid gap-4">
-        <SampleSelect options={options} />
-        {controls}
+      <header className="bg-gray-100 pt-6 pb-4 px-8 border-b border-gray-200 grid gap-4">
+        <div className="flex items-end justify-between">
+          <SampleSelect options={options} />
+          {controls && (
+            <button
+              className="p-2 w-16 hover:bg-gray-200 rounded"
+              onClick={() => setShowControls(!showControls)}
+            >
+              {showControls ? "↑" : "↓"}
+            </button>
+          )}
+        </div>
+        {showControls && controls}
       </header>
       <div className="p-8">{children}</div>
     </>
