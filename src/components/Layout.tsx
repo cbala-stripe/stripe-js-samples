d9dbe617ed194d8c9f8c47684376d42484b796a3
Christopher Henn <christopher@stripe.com>
2021-10-11 14:50:44 -0400

    Remove buggy controls toggle

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import { PAGES } from "../constants";
 import { SampleSelect } from "./SampleSelect";
 
@@ -7,8 +5,6 @@ export const Layout: React.FC<{ controls?: JSX.Element }> = ({
   children,
   controls = null,
 }) => {
-  const [showControls, setShowControls] = useState(true);
-
   const options = PAGES.map(({ title, href }) => ({
     value: href,
     label: title,
@@ -16,19 +12,9 @@ export const Layout: React.FC<{ controls?: JSX.Element }> = ({
 
   return (
     <>
-      <header className="bg-gray-100 pt-6 pb-4 px-8 border-b border-gray-200 grid gap-4">
-        <div className="flex items-end justify-between">
-          <SampleSelect options={options} />
-          {controls && (
-            <button
-              className="p-2 w-16 hover:bg-gray-200 rounded"
-              onClick={() => setShowControls(!showControls)}
-            >
-              {showControls ? "↑" : "↓"}
-            </button>
-          )}
-        </div>
-        {showControls && controls}
+      <header className="bg-gray-100 py-6 px-8 border-b border-gray-200 grid gap-4">
+        <SampleSelect options={options} />
+        {controls}
       </header>
       <div className="p-8">{children}</div>
     </>
