6879b4e7b4dc34a29436074c04d384ff6998e721
Christopher Henn <christopher@stripe.com>
2022-05-13 11:00:32 -0400

    Remove extraneous Netlify config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  target: "serverless",
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/sample/split-card",
-        permanent: false,
-      },
-    ];
-  },
 };
