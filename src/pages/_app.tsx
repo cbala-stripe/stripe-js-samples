242bc84f9f9a190d84d609b8b18b6056ee78723d
Christopher Henn <christopher@stripe.com>
2022-05-13 10:41:21 -0400

    Upgrade Next and React, use StrictMode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,13 @@ import "./_app.css";
 import Head from "next/head";
 
 const App = ({ Component, pageProps }) => {
-  const stripeJsUrl =
-    process.env.NEXT_PUBLIC_STRIPE_JS_URL || "https://js.stripe.com/v3";
+  const stripeJsUrl = process.env.NEXT_PUBLIC_STRIPE_JS_URL;
 
   return (
     <>
       <Head>
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-        <script src={stripeJsUrl}></script>
+        {stripeJsUrl && <script src={stripeJsUrl}></script>}
       </Head>
       <Component {...pageProps} />
     </>
