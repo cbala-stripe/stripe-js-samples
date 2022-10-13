85c52c8db5035135f0d902bec5ef1ccc10d94a47
Christopher Henn <christopher@stripe.com>
2022-10-13 12:13:19 -0400

    Add type and theme controls to PRB sample

--- src/pages/sample/payment-request-button.tsx
+++ src/pages/sample/payment-request-button.tsx
@@ -4,10 +4,36 @@ import { loadStripe } from "@stripe/stripe-js";
 import { useDebugElement } from "../../hooks/useDebugElement";
 import { Layout } from "../../components/Layout";
 import { getCredentials } from "../../helpers/getCredentials";
+import { Field, Select } from "../../components";
+
+const THEME_OPTIONS = [
+  { value: "dark", label: "dark" },
+  { value: "light", label: "light" },
+  { value: "light-outline", label: "light-outline" },
+];
+
+const TYPE_OPTIONS = [
+  { value: "default", label: "default" },
+  { value: "book", label: "book" },
+  { value: "buy", label: "buy" },
+  { value: "donate", label: "donate" },
+  { value: "check-out", label: "check-out" },
+  { value: "subscribe", label: "subscribe" },
+  { value: "reload", label: "reload" },
+  { value: "add-money", label: "add-money" },
+  { value: "top-up", label: "top-up" },
+  { value: "order", label: "order" },
+  { value: "rent", label: "rent" },
+  { value: "support", label: "support" },
+  { value: "contribute", label: "contribute" },
+  { value: "tip", label: "tip" },
+];
 
 const PaymentRequestButtonSample = ({ clientSecret }) => {
   const [ref, setRef] = useState(null);
   const [resultElement, setResult] = useDebugElement();
+  const [theme, setTheme] = useState(THEME_OPTIONS[0].value);
+  const [type, setType] = useState(TYPE_OPTIONS[0].value);
 
   useEffect(() => {
     if (!ref) {
@@ -23,8 +49,12 @@ const PaymentRequestButtonSample = ({ clientSecret }) => {
         total: { label: "Demo total", amount: 100 },
       });
 
+      // @ts-expect-error -- dunno
       const paymentRequestButton = elements.create("paymentRequestButton", {
         paymentRequest: paymentRequest,
+        style: {
+          paymentRequestButton: { type, theme },
+        },
       });
 
       paymentRequest.canMakePayment().then((result) => {
@@ -45,11 +75,26 @@ const PaymentRequestButtonSample = ({ clientSecret }) => {
         e.complete("success");
         setResult(e);
       });
+
+      return () => {
+        paymentRequestButton.destroy();
+      };
     });
-  }, [ref, setResult]);
+  }, [ref, setResult, theme, type]);
 
   return (
-    <Layout>
+    <Layout
+      controls={
+        <>
+          <Field label="type">
+            <Select value={type} onChange={setType} options={TYPE_OPTIONS} />
+          </Field>
+          <Field label="theme">
+            <Select value={theme} onChange={setTheme} options={THEME_OPTIONS} />
+          </Field>
+        </>
+      }
+    >
       <div className="mb-6" ref={setRef} />
       {resultElement}
     </Layout>
