224f4c6c011d271704fbc723d4b77eb3872044fd
Christopher Henn <chris@chrishenn.net>
2023-01-31 11:05:05 -0500

    Add a strict CSP policy

--- next.config.js
+++ next.config.js
@@ -1,3 +1,52 @@
-module.exports = {
-  reactStrictMode: true,
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const config = {
+    reactStrictMode: true,
+
+    async headers() {
+      const csp = {
+        "base-uri": [`'none'`],
+        "object-src": [`'none'`],
+        "frame-src": ["https://js.stripe.com", "https://hooks.stripe.com"],
+        "script-src": [
+          `'self'`,
+          "https://js.stripe.com",
+          "https://maps.googleapis.com",
+        ],
+        "connect-src": [
+          `'self'`,
+          "https://api.stripe.com",
+          "https://maps.googleapis.com",
+        ],
+      };
+
+      if (phase === PHASE_DEVELOPMENT_SERVER) {
+        csp["script-src"].push(`'unsafe-eval'`);
+      }
+
+      const cspHeaderValue = Object.entries(csp).reduce(
+        (acc, [directive, values]) =>
+          `${acc}; ${directive} ${values.join(" ")}`,
+        ""
+      );
+
+      return [
+        {
+          source: "/:path*",
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: cspHeaderValue,
+            },
+          ],
+        },
+      ];
+    },
+  };
+
+  return config;
 };
